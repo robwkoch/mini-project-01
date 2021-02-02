56c97a32233be458e5987346beceb330b84b7158
Jean Ralphio <nateforgille@outlook.com>
2021-02-01 21:48:24 -0600

    pseudo code in JS file

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
 /* Create a home page
 the home page includes: search field, drop down, button
 all contained in a container
+    -jumbotron
+- on button click the 'search results page' opens
+- container on the left side for filtering searches
+    - in the html we create a space for a card to hold the search filter
+    -this becomes a mini homepage
+-container that holds our search results
